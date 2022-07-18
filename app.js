07015d5bab8725072a98fe1cd47faf47990460b9
gejadzeomar <104693200+gejadzeomar@users.noreply.github.com>
2022-07-18 14:19:31 +0400

    1

---
+++ app.js
@@ -0,0 +1,25 @@
+const myName = "Omar";
+ myHobby = "codding";
+ yearOfBirth = 1989;
+ currentYear = 2022;
+
+ let aboutMe = `My name is ${myName}, I am ${currentYear - yearOfBirth}, old and my hobby is ${myHobby}`;
+
+ console.log(aboutMe);
+
+
+ // array
+
+ let myArr = [1, 3, 5, "text", true, false, null, undefined];
+
+ console.log(myArr);
+
+ let firstEl = myArr[0];
+ let secondEl = myArr[1];
+ let myArrLength = myArr.length;
+ let lastEl = myArr[myArrLength - 1];
+
+ console.log(firstEl);
+ console.log(secondEl);
+ console.log(myArrLength);
+ console.log(lastEl);
